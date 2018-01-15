e56da8419c9c6eb5b476015757fabb2c3006b518
Oleksii <kalimdor07@gmail.com>
2018-01-15 19:06:53 +0100

    Tester for comparing files, when you have 5000 lines of query...

--- src/main/java/JavaFun/Tester.java
+++ src/main/java/JavaFun/Tester.java
@@ -1,4 +1,37 @@
 package JavaFun;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Tester {
+    public static void main(String[] args){
+        BufferedReader expBr = null;
+        FileReader expFr = null;
+
+        BufferedReader outBr = null;
+        FileReader outFr = null;
+
+        try {
+            expFr = new FileReader("expected.txt");
+            expBr = new BufferedReader(expFr);
+
+            outFr = new FileReader("output.txt");
+            outBr = new BufferedReader(outFr);
+
+            String expLine = "";
+            String outLine = "";
+            int counter = 1;
+            while ( (expLine = expBr.readLine()) != null) {
+                outLine = outBr.readLine();
+                if(!expLine.equals(outLine))
+                    System.out.println(counter);
+                counter++;
+            }
+        }
+        catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+    }
 }
