87fd443a7aa607b766b46ef73a4083827e42319a
Oleksii <kalimdor07@gmail.com>
2018-01-21 19:45:38 +0100

    Solution. Lesson: do not envent the bycicle!

--- src/main/java/JavaFun/HashSet0.java
+++ src/main/java/JavaFun/HashSet0.java
@@ -15,26 +15,12 @@ public class HashSet0 {
             pair_right[i] = s.next();
         }
 
-        HashSet<String[]> collection = new HashSet<>();
+        HashSet<String> col = new HashSet<>();
 
         for(int i = 0; i < t; i++){
-            String[] pair = new String[2];
-            pair[0] = pair_left[i];
-            pair[1] = pair_right[i];
-
-            if(!contains(pair, collection)){
-                collection.add(pair);
-            }
-            System.out.println(collection.size());
+            col.add(pair_left[i] + "," + pair_right[i]);
+            System.out.println(col.size());
         }
-    }
-
-    public static boolean contains(String[] pair, HashSet<String[]> collection){
-        for (String[] p: collection) {
-            if(p[0].equals(pair[0]) && p[1].equals(pair[1]))
-                return true;
-        }
-        return false;
     }
 
 }
